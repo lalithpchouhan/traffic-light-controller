0ac065d42d19d0c76b6925d1b5f19855f6a5dcdf
lalithpchouhan <lalithpchouhan6@gmail.com>
2025-03-20 22:01:10 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var lights = document.querySelectorAll(".light");
-var currentIndex = 0;
-function changeLight() {
-    lights.forEach(function (light) { return light.classList.remove("active"); });
-    lights[currentIndex].classList.add("active");
-    currentIndex = (currentIndex + 1) % lights.length;
-}
-setInterval(changeLight, 2000);
-changeLight(); // Start the sequence
